29ce85201f3237ca726b87e937a0e50c4834193e
Erlend Ranheim <ranheim38@gmail.com>
2026-04-24 12:54:29 +0200

    Updated image paths again 2

--- src/components/Sesongvelger.jsx
+++ src/components/Sesongvelger.jsx
@@ -4,7 +4,7 @@ import styles from "../css/Sesongvelger.module.css";
 const Sesongvelger = () => {
     return (
         <div className={styles.container}>
-            <img className={styles.logo} src="../../public/Havbruksinfluenserne-logo.png" alt="sd"/>
+            <img className={styles.logo} src="/Havbruksinfluenserne-logo.png" alt="sd"/>
             <button className={styles.button}>Sesong 1</button>
             <button className={styles.button}>Ekstramateriale</button>
         </div>
